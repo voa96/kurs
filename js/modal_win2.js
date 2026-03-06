e86dbd54620a2b9603070303838aa2959fc3942a
vova <drojjingg@gmail.com>
2026-03-06 22:28:26 +0300

    switch fps info

--- js/modal_win2.js
+++ js/modal_win2.js
@@ -22,7 +22,12 @@ const gameInfo = [
         motherBoard: {rec: '', min: ''},
         ssd:{rec:'',min:''},
         powerUnit: {rec: '',min:''},
-        body: {rec: '',min:''}
+        body: {rec: '',min:''},
+        productiveRec: {low: '300 FPS', medium: '200 FPS', high: '100 FPS',max: '30 FPS'},
+        productiveMin: {low: '100 FPS', medium: '80 FPS', high: '50 FPS',max: '20 FPS'},
+        titleName: 'Grand Theft Auto V',
+        img: 'https://i.playground.ru/p/axyE4V_QXmwLFJUJ05q7uQ.jpeg',
+        gameText:'Grand Theft Auto V — это не просто игра, это живая платформа. Несмотря на возраст, благодаря гениальной работе Rockstar и невероятному сообществу моддеров, Лос-Сантос продолжает удивлять. Сегодня в GTA V можно увидеть то, о чем мы не могли и мечтать в 2015 году: фотореалистичные графические моды, дополнения с машинами и оружием нового поколения, полный перезапуск физики и масштабные ролевые проекты.',
     },
     {
         name:'gta6',
@@ -32,43 +37,43 @@ const gameInfo = [
         motherBoard: {rec: '', min: ''},
         ssd:{rec:'',min:''},
         powerUnit: {rec: '',min:''},
-        body: {rec: '',min:''}
+        body: {rec: '',min:''},
+        productiveRec: {low: '300 FPS', medium: '200 FPS', high: '100 FPS',max: '30 FPS'},
+        productiveMin: {low: '100 FPS', medium: '80 FPS', high: '50 FPS',max: '20 FPS'},
+        titleName: 'Grand Theft Auto 6',
+        img: 'https://i.ytimg.com/vi/REP0dcYFP9Q/maxresdefault.jpg',
+        gameText:'Grand Theft Auto VI игра в жанре action-adventure с открытым миром, разрабатываемая компанией Rockstar Games. Станет шестнадцатой по счёту и восьмой крупной игрой в серии Grand Theft Auto. ',
     },
-    {
-        name:'RDR',
-        card: {rec: '5', min : '6',},
-        processor: {rec: '',min: ''},
-        access:{rec: '', min: ''},
-        motherBoard: {rec: '', min: ''},
-        ssd:{rec:'',min:''},
-        powerUnit: {rec: '',min:''},
-        body: {rec: '',min:''}
-    }
 ]
 function switchmin(){
     setTimeout(() => {
         ModalWin.querySelector(".img_choice_min").style.opacity = "1"
         ModalWin.querySelector(".img_choice_rec").style.opacity = "0"
     },10)
-    const oobject = gameInfo.find((item) => item.name === idTarget);
-    ModalWin.querySelector('.card').textContent = oobject.card.rec
+    const gameInfoObject = gameInfo.find((item) => item.name === idTarget);
+    ModalWin.querySelector('.card').textContent = gameInfoObject.card.min
+    ModalWin.querySelector('.productivityLow').textContent = gameInfoObject.productiveMin.low
+    ModalWin.querySelector('.productivityMedium').textContent = gameInfoObject.productiveMin.medium
+    ModalWin.querySelector('.productivityHigh').textContent = gameInfoObject.productiveMin.high
+    ModalWin.querySelector('.productivityMax').textContent = gameInfoObject.productiveMin.max
 }
 function switchmax(){
     setTimeout(() => {
         ModalWin.querySelector(".img_choice_min").style.opacity = "0"
         ModalWin.querySelector(".img_choice_rec").style.opacity = "1"
-    }, 10)
-    const oobject = gameInfo.find((item) => item.name === idTarget);
-    console.log(oobject);
-    
-    ModalWin.querySelector('.card').textContent = oobject.card.min
+    },10)
+    const gameInfoObject = gameInfo.find((item) => item.name === idTarget);
+    console.log(gameInfoObject);
+    ModalWin.querySelector('.card').textContent = gameInfoObject.card.rec
+    ModalWin.querySelector('.productivityLow').textContent = gameInfoObject.productiveRec.low
+    ModalWin.querySelector('.productivityMedium').textContent = gameInfoObject.productiveRec.medium
+    ModalWin.querySelector('.productivityHigh').textContent = gameInfoObject.productiveRec.high
+    ModalWin.querySelector('.productivityMax').textContent = gameInfoObject.productiveRec.max
+
 }
 
-
 CloseModalBtn.addEventListener("click",CloseModal)
-
 function CloseModal(){
-
     ModalWinMain.style.scale = "0.3"
     ModalWin.style.opacity = "0.1"
     setTimeout(() => {
@@ -96,7 +101,6 @@ function createSlider(gameBlock) {
         autoPlayInterval: null,
         isAutoPlaying: true
     };
-    
     function openModalWin(){
         ModalWin.style.display = 'block'
         document.documentElement.style.overflow = 'hidden'
@@ -104,25 +108,21 @@ function createSlider(gameBlock) {
             ModalWinMain.style.scale = '1'
             ModalWin.style.opacity = '1'
         },20);
-        const oobject = gameInfo.find((item) => item.name === idTarget);
-        ModalWin.querySelector('.card').textContent = oobject.card.rec
     }
-
+    
     // Функции для этого слайдера
     function nextSlide() {
-        sliderData.currentSlide++;
-        if (sliderData.currentSlide >= sliderData.slides.length) {
-            sliderData.currentSlide = 0;
-        }
+        sliderData.currentSlide++;  
+        if (sliderData.currentSlide >= sliderData.slides.length) 
+            {sliderData.currentSlide = 0;}
         updateSlider();
         resetAutoPlay();
     }
     
     function prevSlide() {
         sliderData.currentSlide--;
-        if (sliderData.currentSlide < 0) {
-            sliderData.currentSlide = sliderData.slides.length - 1;
-        }
+        if (sliderData.currentSlide < 0) 
+            {sliderData.currentSlide = sliderData.slides.length - 1;}
         updateSlider();
         resetAutoPlay();
     }
@@ -150,11 +150,15 @@ function createSlider(gameBlock) {
     sliderData.nextBtn.addEventListener('click', nextSlide);
     
     sliderData.openModalBtn.addEventListener('click', openModalWin);
-        sliderData.openModalBtn.addEventListener('click', function ltarget(e){
-        idTarget = e.target.id
-        console.log(idTarget);   
-    });
-
+    sliderData.openModalBtn.addEventListener('click', function ltarget(e){
+            idTarget = e.target.id
+            console.log(idTarget);
+            const gameInfoObject = gameInfo.find((item) => item.name === idTarget);
+            ModalWin.querySelector('.game_text').textContent = gameInfoObject.gameText
+            ModalWin.querySelector('.img_game').src = gameInfoObject.img
+            switchmax()
+        });
+        
     
     // Пауза при наведении
     sliderData.container.addEventListener('mouseenter', () => {
